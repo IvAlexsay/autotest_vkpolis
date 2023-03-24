559921cb55f1428d9df08d59458aa3ddf6ff2665
IvAlexsay <ivalexsay@gmail.com>
2023-03-24 23:46:47 +0300

    add send message test

--- src/test/java/OkMessengerTests.java
+++ src/test/java/OkMessengerTests.java
@@ -1,5 +1,6 @@
+import java.util.concurrent.ThreadLocalRandom;
+
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -8,22 +9,25 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
 
 import Templates.OkLogin;
 
-import static com.codeborne.selenide.Selectors.byClassName;
+import static com.codeborne.selenide.Selectors.byAttribute;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class OkMessengerTests extends OkLogin {
-    private static final ElementsCollection dialogs = $(byClassName("custom-scrollbar")).findAll("msg-chats-list-item");
+    private static final ElementsCollection dialogs = $$("msg-chats-list-item");
+    private static final ElementsCollection messages = $$("msg-message");
+    private static final SelenideElement input = $(byAttribute("data-tsid", "write_msg_input-input"));
 
     @BeforeAll
     public static void init(){
         loginIn();
     }
 
-    @Disabled
     @ParameterizedTest
     @Tag("Messenger")
     @DisplayName("Find dialogs with persons")
@@ -34,13 +38,15 @@ public class OkMessengerTests extends OkLogin {
     }
 
 
-    @Disabled
     @Test
     @Tag("Messenger")
+    @DisplayName("Send message")
     public void sendMessage() {
         open("https://ok.ru/messages");
         dialogs.findBy(Condition.text("Алексей Ивакин")).find("a").click();
-        $("msg-input").setValue("sada").pressEnter();
+        String message = "text" + ThreadLocalRandom.current().nextInt();
+        input.val(message).pressEnter();
+        messages.findBy(Condition.text(message)).should(Condition.exist);
     }
 
 }
